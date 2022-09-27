34463ad7e01820e80ab05e5648eb89e3c46aa836
dmitry <dmitrykornev.64@gmail.com>
2022-09-27 15:11:03 +0400

    Исправил ДЗ МАССИВЫ. Часть 1

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
     // ДЗ 1.
@@ -9,13 +12,13 @@ public class Main {
         System.out.println("   Задание 1,2.");
         int[] box = new int[3];
         box[0] = 1;
-        int applesBox = box[0];
         box[1] = 2;
-        int pearsBox = box[1];
         box[2] = 3;
+        int applesBox = box[0];
+        int pearsBox = box[1];
         int tomatoesBox = box[2];
-            for (int i = 0; i < box.length; i++) {
-                System.out.print(box[i]);
+        for (int i = 0; i < box.length; i++) {
+            System.out.print(box[i]);
                 if (i < box.length - 1) {
                     System.out.print(",");
                 }
@@ -36,16 +39,16 @@ public class Main {
 
 
         int[] garage = new int[5];
-        garage[0] = 2;
         int autotruck = garage[0];
-        garage[1] = 3;
         int racing =  garage[1];
-        garage[2] = 4;
         int motorbike = garage[2];
-        garage[3] = 6;
         int tractor = garage[3];
-        garage[4] = 1;
         int bus =  garage[4];
+        garage[0] = 2;
+        garage[1] = 3;
+        garage[2] = 4;
+        garage[3] = 6;
+        garage[4] = 1;
         for (int i = 0; i < garage.length; i++) {
             System.out.print(garage[i]);
             if (i < garage.length - 1) {
@@ -60,12 +63,12 @@ public class Main {
 
         System.out.println("   Задание 2,3.");
         int[] shop = new int[3];
-        shop[0] = 1;
         int applesBox1 = shop[0];
-        shop[1] = 2;
         int pearsBox1 = shop[1];
-        shop[2] = 3;
         int tomatoes1 = shop[2];
+        shop[0] = 1;
+        shop[1] = 2;
+        shop[2] = 3;
         for (int i = shop.length-1; i >= 0 ; i--) {
             System.out.print(shop[i]);
             if (i > shop.length -3) {
@@ -87,16 +90,16 @@ public class Main {
 
 
         int[] parking = new int[5];
-        parking[0] = 2;
         int autotruck1 = parking[0];
-        parking[1] = 3;
         int racing1 =  parking[1];
-        parking[2] = 4;
         int motorbike1 = parking[2];
-        parking[3] = 6;
         int tractor1 = parking[3];
-        parking[4] = 1;
         int bus1 =  parking[4];
+        parking[0] = 2;
+        parking[1] = 3;
+        parking[2] = 4;
+        parking[3] = 6;
+        parking[4] = 1;
         for (int i = parking.length-1; i >= 0 ; i--) {
             System.out.print(parking[i]);
             if (i > parking.length -5) {
@@ -107,55 +110,38 @@ public class Main {
 
 
         // Задание 2,4.
-     // задание 4 у меня не молучилось сделать, в материалах не объяснялись данные процессы (
         System.out.println("   Задание 2,4");
         int[] boxes = new int[3];
-        boxes[0] = 1;
         int apples = boxes[0];
-        boxes[1] = 2;
         int pears = boxes[1];
-        boxes[2] = 3;
         int tomatoes = boxes[2];
+        boxes[0] = 1;
+        boxes[1] = 2;
+        boxes[2] = 3;
         for (int r = 0; r < boxes.length; r++) {
-            if (r % 2 != 0) {
-                    boxes[r]++;
-                    System.out.print(boxes[r]++);
-                    System.out.print(",");
+            if (boxes[r] % 2 != 0) {
+                boxes[r] += 1;
             }
         }
-        System.out.println();
-
-
-
-        float[] mass = {1.57f, 7.654f, 9.986f};
-        for (int w = 0; w < mass.length; w++) {
-            System.out.print(mass[w]);
-            if (w < mass.length - 1) {
-                System.out.print(",");
-            }
-        }
-
-        System.out.println();
+        System.out.println(Arrays.toString(boxes));
 
 
         int[] camp = new int[5];
-        camp[0] = 2;
         int tents = camp[0];
-        camp[1] = 3;
         int sleepingBags =  camp[1];
-        camp[2] = 4;
         int blankets = camp[2];
-        camp[3] = 6;
         int shovels = camp[3];
-        camp[4] = 1;
         int ration =  camp[4];
-        for (int i = 0; i < garage.length; i++) {
-            System.out.print(garage[i]);
-            if (i < garage.length - 1) {
-                System.out.print(",");
+        camp[0] = 2;
+        camp[1] = 3;
+        camp[2] = 4;
+        camp[3] = 6;
+        camp[4] = 1;
+        for (int e = 0; e < garage.length; e++) {
+            if (camp[e] % 2!=0) {
+                camp[e] +=1 ;
             }
         }
-        System.out.println();
-
+        System.out.println(Arrays.toString(camp));
     }
 }
